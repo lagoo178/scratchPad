57547ec8fe58083182ea0c2040ce24f26b86e067
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-03-17 14:07:23 +0700

    animation test 1

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -5,14 +5,34 @@ class SceneMain extends Phaser.Scene {
     preload()
     {
     	//load our images or sounds 
-        this.load.image("face", "images/face.png");
+        //this.load.image("face", "images/face.png");
+        
+        this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
     }
     create() {
        //define our objects
-       this.face = this.add.image(0, 0, "face");
-       this.face.setOrigin(0.5,0.5);
-       this.face.x = game.config.width/2;
-       this.face.y = game.config.height/2;
+       
+       this.char = this.add.sprite(game.config.width/2,game.config.height/2,"boy");
+       this.anims.create({
+            key: 'walk',
+            frames: [
+                { key: 'boy',frame:0 },
+                { key: 'boy',frame:1 },
+                { key: 'boy',frame:2 },
+                { key: 'boy',frame:3 },
+                { key: 'boy',frame:4 },
+                { key: 'boy',frame:5 },
+            ],
+            frameRate: 10,
+            repeat: -1
+        });
+       this.char.play('walk');
+
+
+       //this.face = this.add.image(0, 0, "face");
+       //this.face.setOrigin(0.5,0.5);
+       //this.face.x = game.config.width/2;
+       //this.face.y = game.config.height/2;
        //this.face.alpha = 0.5;
        //this.face.scaleX = 0.5;
        //this.face.scaleY = 0.5;
